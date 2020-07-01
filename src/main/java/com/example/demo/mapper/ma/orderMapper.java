a27b04d7eb8891d61405048c8a4d3bb14c9dfc71
maxuhuii <44050436+maxuhuii@users.noreply.github.com>
2020-07-01 21:46:25 +0800

    在写收货人信息动态变化

---
+++ src/main/java/com/example/demo/mapper/ma/orderMapper.java
@@ -0,0 +1,49 @@
+package com.example.demo.mapper.ma;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+import java.util.Map;
+
+//映射类，用来操作数据库
+@Mapper //用这个知道他是映射类，自动产生对象，执行相关操作
+public interface orderMapper {
+    //表里面的数据有很多行，用list来做
+    //用map以列表作为对，列表作为value
+    //数据库相关语言从那个软件设计好后直接复制过来就好 不用硬写
+    @Select("select * from order_info")
+    public List<Map<String,Object>> findAll();
+
+    /**
+     * #{n} 表示一个占位符，用于通过变量赋值
+     * @param id
+     * @return
+     *
+     */
+    @Select("select * from order_info where order_id=#{n}")
+    public Map<String,Object> findById(int id);
+
+    //插入数据 有什么内容就要插什么
+    //从上面的变成下面的,传递一个map,map里用string是因为从页面传过来的数据都是string类型的
+    //public void insert(String name,String sex,int id)
+    @Insert("insert into order_info(order_id,address_id,cart_id,order_number,time,state,type,goods_id,user_id,amount)" +
+            "value(#{order.order_id},#{order.address_id},#{order.cart_id},#{order.order_number},#{order.time},#{order.state},#{order.type},#{order.goods_id},#{order.user_id},#{order.amount})")
+    public int insert(@Param("order") Map<String, String> order);//@Param("tea") 表示参数的别名，在sql语句中使用
+
+    /**
+     * 根据订单号来改数量
+     *
+     * 只要传递多个参数的都可以使用map集合进行封装，可以减少方法的参数个数
+     */
+    //修改密码
+    @Update("update order_info" +
+            " set amount=#{o.amount}" +
+            " where order_number=#{o.order_number}")
+    public int update(@Param("o") Map<String, String> map);
+
+
+    //如果想通过传入表名来找到表中信息，可用${table}来赋值给table
+    //通过id删除用户
+    @Delete("delete from order_info where order_id=#{id}")
+    public int delete(int id);
+}
