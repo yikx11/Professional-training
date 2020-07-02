9aa0bb1b49cb30756b1d705205cb1dbb13de2338
maxuhuii <44050436+maxuhuii@users.noreply.github.com>
2020-07-02 14:28:09 +0800

    图片上传做完了，上传到upfile中

---
+++ src/main/java/com/example/demo/controller/ma/FileController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller.ma;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+public class FileController {
+
+    @RequestMapping("/uploadimg")
+    public String fileUp(MultipartFile file, @RequestParam Map<String, String> user){
+        String msg="上传失败";
+        if(file.isEmpty()){
+            return "请选择上传的文件";
+        }
+        String fileName=file.getOriginalFilename();
+        String u_name=user.get("u_name");
+        System.out.println(u_name);
+        String path= System.getProperty("user.dir")+"\\upfile";
+        File f=new File(path+"\\"+fileName);
+        if(!f.getParentFile().exists()){
+            f.getParentFile().mkdir();
+        }
+        try {
+            file.transferTo(f);
+            msg="上传成功";
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return msg;
+    }
+}
